a5067fac66a15c4102351ebe244d928d3ee4530f
Alex Flok <alexflok2018@gmail.com>
2025-02-02 21:21:22 -0800

    wip: add react hook smart form;

--- src/components/RFForm/RFForm.tsx
+++ src/components/RFForm/RFForm.tsx
@@ -1,14 +1,19 @@
 import RFRow, { RFRowProps } from "../RFRow/RFRow"
 import RFSubmit, { RFSubmitProps } from "../RFSubmit/RFSubmit"
+import {useForm} from "react-hook-form"
 
 type FormProps = {
     form: RFRowProps[]
+    onSubmit: () => void
     submit?: RFSubmitProps
 }
 
-export default function RFForm({form, submit}: FormProps){
-    return <>
+export default function RFForm({form, submit, onSubmit}: FormProps){
+
+    const {handleSubmit} = useForm({defaultValues: {}})
+
+    return <form onSubmit={handleSubmit(onSubmit)}>
         {form.map(rowProps => <RFRow {...rowProps} />)}
         {(form.length > 0 || submit?.label) && <RFSubmit {...submit} />}
-    </>
+        </form>
 }
